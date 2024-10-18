d7e677c2d1580b497b938ba2ecc9768e326e62e2
Sumit <sumit1711kd@gmail.com>
2024-10-19 03:45:07 +0530

    Modify folder structure

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-import express from 'express'
+import express from 'express';
 import dotenv from 'dotenv';
 import connect from './config/dbConfig.js';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
-import bidRoutes from "./routes/bidRoutes.js"
-import cors from 'cors'
-import path from 'path'
-
+import bidRoutes from "./routes/bidRoutes.js";
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 const app = express();
@@ -14,26 +15,25 @@ app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Hello Sumit");
-})
+});
 
 app.use('/api/users', userRoutes);
-
 app.use("/api/products", productRoutes);
-
 app.use('/api/bids', bidRoutes);
 
-__dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")));
-    app.get("*", (req,res)=>{
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    })
+    });
 }
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     connect();
-    console.log("Server started successfully");
-})
+    console.log("Server started successfully on port", PORT);
+});
