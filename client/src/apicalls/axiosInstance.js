16ba71c531b5193ccbabf417177d1ef77e3a99d8
Sumit <sumit1711kd@gmail.com>
2024-10-20 19:29:34 +0530

    Change url

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
+    baseURL: 'https://e-comm-biding.onrender.com', 
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`,
     }
