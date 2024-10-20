63041dd6f24ed1b2258bcd0e5f74a8d451cbdc2b
Sumit <sumit1711kd@gmail.com>
2024-10-20 19:34:03 +0530

    Remove baseURL

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://e-comm-biding.onrender.com', 
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`,
     }
